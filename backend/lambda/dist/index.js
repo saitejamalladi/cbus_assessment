359eb6670ef64c011807aa88f536fd4b98893928
Sai Teja <saiteja@local>
2026-02-18 21:58:52 +1100

    chore: ignore lambda dist output

--- backend/lambda/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCustomersHandler = void 0;
-var getCustomers_1 = require("./handlers/getCustomers");
-Object.defineProperty(exports, "getCustomersHandler", { enumerable: true, get: function () { return getCustomers_1.handler; } });
